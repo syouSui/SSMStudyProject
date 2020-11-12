487b3bf22de889f5e7c605adc0ab13cc36da7cd1
syouSui <justTouSan@gmail.com>
2020-11-12 09:45:13 +0800

    补充上落下的main目录.

--- Mybatis_Spring/src/main/test/com/syousui/test/DaoTest.java
+++ Mybatis_Spring/src/main/test/com/syousui/test/DaoTest.java
@@ -1,4 +1,5 @@
 package com.syousui.test;
+
 import com.syousui.dao.CustomerDao;
 import com.syousui.mapper.CustomerMapper;
 import com.syousui.po.Customer;
@@ -11,31 +12,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class DaoTest {
 
-//    public static void main ( String[] args ) {
-//        findCustomerByIdDaoTest();
-//    }
-
     @Test
-    public void findCustomerByIdDaoTest(){
+    public void findCustomerByIdDaoTest ( ) {
 //	@Test
 //	public static void findCustomerByIdDaoTest(){
-		ApplicationContext act = 
-		    new ClassPathXmlApplicationContext("applicationContext.xml");
-          // 根据容器中Bean的id来获取指定的Bean
-	     CustomerDao customerDao =
-                              (CustomerDao) act.getBean("customerDao");
+        ApplicationContext act = new ClassPathXmlApplicationContext( "applicationContext.xml" );
+        // 根据容器中Bean的id来获取指定的Bean
+        CustomerDao customerDao = (CustomerDao) act.getBean( "customerDao" );
 //	     CustomerDao customerDao = act.getBean(CustomerDao.class);
-		 Customer customer = customerDao.findCustomerById(1);
-		 System.out.println(customer);
-	}
-	
-//	@Test
-	public static void findCustomerByIdMapperTest(){
-	    ApplicationContext act = 
-	            new ClassPathXmlApplicationContext("applicationContext.xml");
-	    CustomerMapper customerMapper = act.getBean( CustomerMapper.class);
-	    Customer customer = customerMapper.findCustomerById(1);
-	    System.out.println(customer);
-	}
+        Customer customer = customerDao.findCustomerById( 1 );
+        System.out.println( customer );
+    }
+
+    @Test
+    public void findCustomerByIdMapperTest ( ) {
+        ApplicationContext act = new ClassPathXmlApplicationContext( "applicationContext.xml" );
+        CustomerMapper customerMapper = act.getBean( CustomerMapper.class );
+        Customer customer = customerMapper.findCustomerById( 1 );
+        System.out.println( customer );
+    }
 
 }
