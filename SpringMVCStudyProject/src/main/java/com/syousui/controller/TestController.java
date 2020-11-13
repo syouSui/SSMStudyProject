124285b164cc7612f6039d507a275962e8fd0280
syouSui <justTouSan@gmail.com>
2020-11-13 11:27:05 +0800

    完善RESTful风格.

--- SpringMVCStudyProject/src/main/java/com/syousui/controller/TestController.java
+++ SpringMVCStudyProject/src/main/java/com/syousui/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.syousui.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,8 +35,18 @@ public class TestController {
         response.getWriter( ).println( data );
     }
 
-    @RequestMapping ( value = "/var/{num}", method = RequestMethod.GET )
-    public void path_auto ( @PathVariable ( "num" ) String num, HttpServletResponse response ) throws IOException {
+    @RequestMapping ( value = "/var1/{num}", method = RequestMethod.GET )
+    public void path_auto_requestMapping ( @PathVariable ( "num" ) String num, HttpServletResponse response ) throws IOException {
         response.getWriter( ).println( num );
     }
+
+    @GetMapping ( value = "/var2/{num}" )
+    public void path_auto_getMapping ( @PathVariable ( "num" ) String num, HttpServletResponse response ) throws IOException {
+        response.getWriter( ).println( num );
+    }
+//    使用 RESTful 风格. 以下分别对应增删改查, 相当于 RequestMapping设置method属性.
+//    @PostMapping
+//    @DeleteMapping
+//    @PutMapping
+//    @GetMapping
 }
