392ed4b66aa06c50d2a0348a41fbfecf5831f132
syouSui <justTouSan@gmail.com>
2020-11-13 08:32:06 +0800

    添加RESTful风格实现.

--- SpringMVCStudyProject/src/main/java/com/syousui/controller/TestController.java
+++ SpringMVCStudyProject/src/main/java/com/syousui/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.syousui.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author : acmaker
@@ -20,14 +23,19 @@ import javax.servlet.http.HttpServletResponse;
 public class TestController {
 
     @RequestMapping ( value = "/test" )
-    public void test ( HttpServletRequest request, HttpServletResponse response ) throws Exception {
+    public void test ( HttpServletResponse response ) throws Exception {
         System.out.println( "/Test/test" );
         response.getWriter( ).print( "Spring Test!" );
     }
 
     @RequestMapping ( value = "/auto" )
-    public void auto ( HttpServletRequest request, HttpServletResponse response ) throws Exception {
+    public void var_auto ( HttpServletRequest request, HttpServletResponse response ) throws Exception {
         String data = request.getParameter( "var" );
         response.getWriter( ).println( data );
+    }
+
+    @RequestMapping ( value = "/var/{num}", method = RequestMethod.GET )
+    public void path_auto ( @PathVariable ( "num" ) String num, HttpServletResponse response ) throws IOException {
+        response.getWriter( ).println( num );
     }
 }
