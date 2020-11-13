c2a6dda524a36ab22f2fe93999da6772a23c5787
syouSui <justTouSan@gmail.com>
2020-11-13 13:59:24 +0800

    ***完善了Mybatis根据interface和mapper自动生成类的测试.

--- Mybatis_Spring/src/main/test/com/syousui/test/DaoTest.java
+++ Mybatis_Spring/src/main/test/com/syousui/test/DaoTest.java
@@ -12,20 +12,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class DaoTest {
 
+    /**
+     * Dao的开发模式测试
+     */
     @Test
     public void findCustomerByIdDaoTest ( ) {
         ApplicationContext act = new ClassPathXmlApplicationContext( "applicationContext.xml" );
-//        根据容器中Bean的id来获取指定的Bean
-        CustomerDao customerDao = (CustomerDao) act.getBean( "customerDao" );
+        CustomerDao customerDao = act.getBean( "customerDao", CustomerDao.class );
 //        CustomerDao customerDao = act.getBean( CustomerDao.class );
         Customer customer = customerDao.findCustomerById( 1 );
         System.out.println( customer );
     }
 
+    /**
+     * Mapper的开发模式测试
+     */
     @Test
     public void findCustomerByIdMapperTest ( ) {
         ApplicationContext act = new ClassPathXmlApplicationContext( "applicationContext.xml" );
-        CustomerMapper customerMapper = act.getBean( CustomerMapper.class );
+        CustomerMapper customerMapper = act.getBean( "customerMapper", CustomerMapper.class );
+//        CustomerMapper customerMapper = act.getBean( CustomerMapper.class );
         Customer customer = customerMapper.findCustomerById( 1 );
         System.out.println( customer );
     }
