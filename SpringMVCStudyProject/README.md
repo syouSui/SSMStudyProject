496a2ed6061af91e73fad2977116f128717b77c1
syouSui <justTouSan@gmail.com>
2020-11-13 10:33:42 +0800

    完成文件上传下载功能.

--- SpringMVCStudyProject/README.md
+++ SpringMVCStudyProject/README.md
@@ -3,3 +3,6 @@
 
 # Introduction
 只包含 JSON 数据交互的模式, 剔除所有 jsp 相关的内容.
+
+- 文件上传下载
+- RESTful 风格
---
+++ SpringMVCStudyProject/src/main/java/com/syousui/controller/FileUploadController.java
@@ -0,0 +1,102 @@
+package com.syousui.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 文件上传
+ */
+@Controller
+public class FileUploadController {
+    /**
+     * 执行文件上传
+     */
+    @RequestMapping ( "/fileUpload" )
+    public void handleFormUpload ( @RequestParam ( "person" ) String personName,
+                                   @RequestParam ( "uploadFiles" ) List<MultipartFile> uploadFiles,
+                                   HttpServletRequest request,
+                                   HttpServletResponse response ) throws IOException {
+        // 判断所上传文件是否存在
+        if ( uploadFiles.isEmpty( ) ) {
+            response.setContentType( "text/html" );
+            response.setCharacterEncoding( "utf-8" );
+            response.getWriter( ).println( "文件不能为空!请重新上传!" );
+        }
+        //循环输出上传的文件
+        for ( MultipartFile file : uploadFiles ) {
+            // 设置上传文件的保存地址目录
+            File filePath = new File( request.getServletContext( ).getRealPath( "/upload/" ) );
+            // 如果保存文件的地址不存在，就先创建目录
+            if ( !filePath.exists( ) ) filePath.mkdirs( );
+
+            // 使用UUID重新命名上传的文件名称(上传人_UUID_原文件名称), 并使用MultipartFile接口的方法完成文件上传到指定位置
+            file.transferTo( new File(
+                            filePath + File.separator +
+                                    personName + "_" + UUID.randomUUID( ) + "_" + file.getOriginalFilename( )
+                    )
+            );
+        }
+        response.setContentType( "text/html" );
+        response.setCharacterEncoding( "utf-8" );
+        response.getWriter( ).println( "文件上传成功!" );
+    }
+
+    @RequestMapping ( "/download" )
+    public ResponseEntity<byte[]> fileDownload ( String fileName, HttpServletRequest request ) throws IOException {
+        // 创建该文件对象
+        File file = new File( request.getServletContext( ).getRealPath( "/upload/" ) + File.separator + fileName );
+
+        // 对文件名编码，防止中文文件乱码
+        fileName = this.handleFilNameEncoding( fileName, request );
+
+        // 设置响应头
+        HttpHeaders headers = new HttpHeaders( );
+        // 通知浏览器以下载的方式打开文件
+        headers.setContentDispositionFormData( "attachment", fileName );
+        // 定义以流的形式下载返回文件数据
+        headers.setContentType( MediaType.APPLICATION_OCTET_STREAM );
+
+        // 使用Spring MVC框架的ResponseEntity对象封装返回下载数据
+        return new ResponseEntity<byte[]>(
+                FileUtils.readFileToByteArray( file ),
+                headers,
+                HttpStatus.OK
+        );
+    }
+    /**
+     * 根据浏览器的不同进行编码设置，返回编码后的文件名
+     */
+    public String handleFilNameEncoding ( String fileName, HttpServletRequest request ) throws UnsupportedEncodingException {
+        // IE不同版本User-Agent中出现的关键词
+        String[] IEBrowserKeyWords = { "MSIE", "Trident", "Edge" };
+        // 获取请求头代理信息
+        String userAgent = request.getHeader( "User-Agent" );
+        for ( String keyWord : IEBrowserKeyWords ) {
+            if ( userAgent.contains( keyWord ) ) {
+                //IE内核浏览器，统一为UTF-8编码显示
+                return URLEncoder.encode( fileName, "UTF-8" );
+            }
+        }
+        //火狐等其它浏览器统一为ISO-8859-1编码显示
+        return new String( fileName.getBytes( StandardCharsets.UTF_8 ), StandardCharsets.ISO_8859_1 );
+    }
+
+
+}
